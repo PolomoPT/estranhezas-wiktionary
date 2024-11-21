f97a146dca1854e064aceabdba5014d7754d16db
Polomo <121315943+PolomoPT@users.noreply.github.com>
2024-11-21 19:42:31 -0300

    código éi-ói

--- éi-ói/código.py
+++ éi-ói/código.py
@@ -5,8 +5,6 @@ output_file_path = 'éi-ói\\com_ei_oi.txt'
 
 with open(input_file_path, 'r', encoding='utf-8') as input_file, open(output_file_path, 'w', encoding='utf-8') as output_file:
     for line in input_file:
-        match_éi = re.search('éi..', line)
-        match_ói = re.search('ói..', line)
-        match_suffix = re.search('^-', line)
-        if match_éi or match_ói:
+        match_éi = re.search('(éi|ói)(?=.)(?!s )(?!s$)', line)
+        if match_éi:
             output_file.write("|"+line)
