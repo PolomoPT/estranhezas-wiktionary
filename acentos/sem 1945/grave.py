72fd440712dae64b039b2245235e77fbc2e75a7a
Polomo <121315943+PolomoPT@users.noreply.github.com>
2024-12-06 07:50:12 -0300

    aa

--- acentos/sem 1945/grave.py
+++ acentos/sem 1945/grave.py
@@ -1,10 +1,10 @@
 import re
 
-input_file_path = 'acentos\\com 1945\\lemmas.txt'
-output_file_path = 'acentos\\com 1974\\com_mente.txt'
+input_file_path = 'acentos\\sem 1945\\lemmas.txt'
+output_file_path = 'acentos\\sem 1945\\com_mente.txt'
 
 with open(input_file_path, 'r', encoding='utf-8') as input_file, open(output_file_path, 'w', encoding='utf-8') as output_file:
     for line in input_file:
-        match = re.search('[à|è|ì|ò|ù].*mente', line)
+        match = re.search('[á|é|í|ó|ú]mente', line)
         if match:
             output_file.write("|"+line)
