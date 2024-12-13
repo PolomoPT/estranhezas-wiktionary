051d4942316174d7da34dabcf06e663e5aae444f
Polomo <121315943+PolomoPT@users.noreply.github.com>
2024-12-13 06:24:07 -0300

    novos programas

--- 1911 ou 1943/criar lemmas.py
+++ 1911 ou 1943/criar lemmas.py
@@ -26,11 +26,9 @@ try:
         sql = """
         SELECT DISTINCT page_title 
         FROM page 
-        JOIN categorylinks cl1 ON page_id = cl1.cl_from 
-        JOIN categorylinks cl2 ON page_id = cl2.cl_from 
+        JOIN categorylinks ON page_id = cl_from 
         WHERE page_namespace = 0 
-        AND cl1.cl_to = "Portuguese_lemmas" 
-        AND cl2.cl_to IN ("Portuguese_forms_superseded_in_1911", "Portuguese_forms_superseded_in_1943")
+        AND cl_to IN ("Portuguese_forms_superseded_in_1911", "Portuguese_forms_superseded_in_1943")
         ORDER BY page_title;"""
 
         cursor.execute(sql)
--- sem Brazil 1990/criar lemmas.py
+++ sem Brazil 1990/criar lemmas.py
@@ -24,19 +24,19 @@ try:
     with connection.cursor(dictionary=True) as cursor:
         # Get all Portuguese lemmas not in pre-1990 categories
         sql = """
-SELECT DISTINCT page_title 
-FROM page 
-JOIN categorylinks ON page_id = cl_from 
-WHERE page_namespace = 0 
-AND cl_to = "Portuguese_lemmas" 
-AND page_id NOT IN (
-    SELECT cl_from 
-    FROM categorylinks 
-    WHERE cl_to IN (
-        "Brazilian Portuguese forms superseded by AO1990"
-    )
-)
-ORDER BY page_title;"""
+        SELECT DISTINCT page_title
+        FROM page
+        JOIN categorylinks ON page_id = cl_from
+        WHERE page_namespace = 0
+        AND page_id NOT IN (
+            SELECT cl_from 
+            FROM categorylinks 
+            WHERE cl_to IN (
+                "Brazilian Portuguese forms superseded by AO1990"
+            )
+        )
+        ORDER BY page_title;"""
+
         cursor.execute(sql)
         results = cursor.fetchall()
         with open("sem Brazil 1990\\lemmas.txt", "w", encoding="utf-8") as file:
