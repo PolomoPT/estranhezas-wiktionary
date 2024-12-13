00fc72e445958b2e46580a543fdbdffc2fb50827
Polomo <121315943+PolomoPT@users.noreply.github.com>
2024-12-13 06:59:14 -0300

    sem brazil 1990

--- sem Brazil 1990/dierese.py
+++ sem Brazil 1990/dierese.py
@@ -1,7 +1,7 @@
 import re
 
-input_file_path = 'acentos\sem Brazil 1990\lemmas.txt'
-output_file_path = 'acentos\sem Brazil 1990\com_ü_etc.txt'
+input_file_path = 'sem Brazil 1990\\lemmas.txt'
+output_file_path = 'sem Brazil 1990\\com_ü_etc.txt'
 
 with open(input_file_path, 'r', encoding='utf-8') as input_file, open(output_file_path, 'w', encoding='utf-8') as output_file:
     for line in input_file:
