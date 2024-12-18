cb952bf1c808a10e2b330ad1e68dd0a35fd76490
Polomo <121315943+PolomoPT@users.noreply.github.com>
2024-12-17 22:16:42 -0300

    atualizar listas

--- sem 1945/programa/grave.py
+++ sem 1945/programa/grave.py
@@ -5,6 +5,6 @@ output_file_path = 'sem 1945\\com_mente.txt'
 
 with open(input_file_path, 'r', encoding='utf-8') as input_file, open(output_file_path, 'w', encoding='utf-8') as output_file:
     for line in input_file:
-        match = re.search('[á|é|í|ó|ú]\S*mente', line)
+        match = re.search('[á|é|í|ó|ú][^-\s]*mente', line)
         if match:
             output_file.write("|"+line)
--- update everything.py
+++ update everything.py
@@ -8,6 +8,14 @@ try:
 except Exception as e:
     print(f"Error executing '1911 1943\\programa\\criar lista.py': {e}")
 
+# 1943 Portugal 1990
+try:
+    with open("1943 Portugal 1990\\programa\\criar lista.py", encoding='utf-8') as f:
+        exec(f.read())
+    print("Executed 'criar lista.py' in 1943 Portugal 1990")
+except Exception as e:
+    print(f"Error executing '1911 1943\\programa\\criar lista.py': {e}")
+
 # sem 1931
 try:
     with open("sem 1931\\programa\\criar lista.py", encoding='utf-8') as f:
